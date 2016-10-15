9077363c00d360064e639cbee52e6fa09915b3d1
waliente <waliente@gmail.com>
2016-10-15 10:00:07 +0200

    main

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.use('/node_modules', express.static(__dirname + './node_modules'));
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers");
     next();
 });
 
